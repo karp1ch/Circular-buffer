47d2b6c6d6bcbf47a194beb9b4bc96c456d64c2f
Aliaksandr <106777205+karp1ch@users.noreply.github.com>
2022-09-16 00:55:13 +0200

    Add files via upload

---
+++ circled_buffer/circled_buffer.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+#define BufferSize 5                                                            // size of buffer
+
+struct {                                                                        // stucture of buffer
+    int readIndex = 0;
+    int writeIndex = 0;
+    bool isFull = 0;
+    bool isEmpty = 1;
+    int data[BufferSize];
+}buffer;
+
+
+void put() {                                                                    // data entry
+    if (buffer.isFull != 1) {
+        system("cls");
+        std::cout << "Enter data [" << buffer.writeIndex + 1 << "]" << std::endl;
+        std::cin >> buffer.data[buffer.writeIndex];
+        buffer.writeIndex++;
+    }
+    else {
+        system("cls");
+        std::cout << "Buffer is full" << std::endl;
+
+    }
+    
+}
+
+void get() {                                                                    // data output
+    if (buffer.isEmpty != 1) {
+        system("cls");
+        std::cout << '[' << buffer.readIndex + 1 << "] : ";
+        std::cout << buffer.data[buffer.readIndex] << std::endl;
+        buffer.readIndex++;
+    }
+    else {
+        system("cls");
+        std::cout << "Buffer is empty" << std::endl;
+
+    }
+}
+
+int main()
+{
+
+    while (true) {
+
+        int command; 
+
+
+        if (buffer.writeIndex >= 5)                                             // conditions
+            buffer.isFull = 1;
+        
+        else {
+            buffer.isFull = 0;
+            if (buffer.readIndex == 5)
+                buffer.writeIndex = 0;
+        }
+
+        if (buffer.writeIndex <= buffer.readIndex || buffer.readIndex == 5) {
+            buffer.isEmpty = 1;
+            buffer.writeIndex = 0;
+            buffer.readIndex = 0;
+        }
+        else 
+            buffer.isEmpty = 0;
+
+
+        do {                                                                    // inputing command     
+            std::cout << "Enter the command (0 - input data, 1 - output data)" << std::endl;
+            std::cin >> command;
+        } while (command != 0 && command != 1);
+
+        if (command == 0)
+            put();
+        else if (command == 1)
+            get();  
+    }
+
+}
