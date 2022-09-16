495d47679d5167f210aa0d8e1f5996468fd70050
Aliaksandr <106777205+karp1ch@users.noreply.github.com>
2022-09-16 13:16:43 +0200

    Update circled_buffer.cpp

--- circled_buffer/circled_buffer.cpp
+++ circled_buffer/circled_buffer.cpp
@@ -2,72 +2,72 @@
 
 #define BufferSize 5                                                            // size of buffer
 
-struct {                                                                        // stucture of buffer
+typedef struct buf{                                                                        // stucture of buffer
     int readIndex = 0;
     int writeIndex = 0;
     bool isFull = 0;
     bool isEmpty = 1;
     int data[BufferSize];
-}buffer;
+}CircularBuffer;
 
 
 
-bool putBuf() {
+bool putBuf(CircularBuffer *buffer) { // 0 - failed, 1 - success
 
-    if (buffer.isFull) {
+    if (buffer->isFull) {
         system("cls");
         std::cout << "Buffer is full" << std::endl;
         return 0;
     }
 
-    if (buffer.writeIndex >= BufferSize)
-        buffer.writeIndex = 0;
+    if (buffer->writeIndex >= BufferSize)
+        buffer->writeIndex = 0;
 
-    if (buffer.isEmpty) {
+    if (buffer->isEmpty) {
 
-        buffer.isEmpty = 0;
+        buffer->isEmpty = 0;
 
         system("cls");
-        std::cout << "Enter data [" << buffer.writeIndex + 1 << "]" << std::endl;
-        std::cin >> buffer.data[buffer.writeIndex++];
+        std::cout << "Enter data [" << buffer->writeIndex + 1 << "]" << std::endl;
+        std::cin >> buffer->data[buffer->writeIndex++];
 
-        if (buffer.writeIndex == buffer.readIndex)
-            buffer.isFull = 1;
+        if (buffer->writeIndex == buffer->readIndex)
+            buffer->isFull = 1;
 
         return 1;
     }
 
     system("cls");
-    std::cout << "Enter data [" << buffer.writeIndex + 1 << "]" << std::endl;
-    std::cin >> buffer.data[buffer.writeIndex++];
+    std::cout << "Enter data [" << buffer->writeIndex + 1 << "]" << std::endl;
+    std::cin >> buffer->data[buffer->writeIndex++];
 
-    if (buffer.writeIndex == buffer.readIndex)
-        buffer.isFull = 1;
+    if (buffer->writeIndex == buffer->readIndex)
+        buffer->isFull = 1;
 
     return 1;
 
 
 }
 
-bool getBuf() {
+bool getBuf(CircularBuffer *buffer) {
 
-    if (buffer.isEmpty) {
+    if (buffer->isEmpty) {
         system("cls");
         std::cout << "Buffer is empty" << std::endl;
         return 0;
     }
 
-    if (buffer.readIndex >= BufferSize)
-        buffer.readIndex = 0;
+    if (buffer->readIndex >= BufferSize)
+        buffer->readIndex = 0;
 
-    buffer.isFull = 0;
+    buffer->isFull = 0;
 
     system("cls");
-    std::cout << '[' << buffer.readIndex + 1 << "] : ";
-    std::cout << buffer.data[buffer.readIndex++] << std::endl;
+    std::cout << '[' << buffer->readIndex + 1 << "] : ";
+    std::cout << buffer->data[buffer->readIndex++] << std::endl;
 
-    if (buffer.readIndex == buffer.writeIndex)
-        buffer.isEmpty = 1;
+    if (buffer->readIndex == buffer->writeIndex)
+        buffer->isEmpty = 1;
 
     return 1;
 
@@ -77,10 +77,11 @@ bool getBuf() {
 
 int main()
 {
+    CircularBuffer buffer;
 
     while (true) {
 
-        int command; 
+        int command;
 
         do {                                                                    // inputing command     
             std::cout << "Enter the command (0 - input data, 1 - output data)" << std::endl;
@@ -88,20 +89,20 @@ int main()
         } while (command != 0 && command != 1);
 
         if (command == 0)
-            putBuf();
+            putBuf(&buffer);
         else if (command == 1)
-            getBuf();  
+            getBuf(&buffer);  
 
 
-        /*std::cout << std::endl << std::endl;                                   // debugging
-        for (int i = 0; i < BufferSize; i++)
-            std::cout << buffer.data[i] << ' ' << std::endl;
-        std::cout << "Is empty " << buffer.isEmpty << std::endl;
-        std::cout << "Is full " << buffer.isFull << std::endl;
-        std::cout << "Read index " << buffer.readIndex << std::endl;
-        std::cout << "Write index " << buffer.writeIndex << std::endl;*/
+        //std::cout << std::endl << std::endl;                                   // debugging
+        //for (int i = 0; i < BufferSize; i++)
+        //    std::cout << buffer.data[i] << ' ' << std::endl;
+        //std::cout << "Is empty " << buffer.isEmpty << std::endl;
+        //std::cout << "Is full " << buffer.isFull << std::endl;
+        //std::cout << "Read index " << buffer.readIndex << std::endl;
+        //std::cout << "Write index " << buffer.writeIndex << std::endl;
 
-
+        
 
     }
 
